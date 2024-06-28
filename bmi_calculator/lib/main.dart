aec83d872aa765c4e1615b1e15c33fb2bf13e504
Andrés Velasquez <andresvelasquez@Andress-MacBook-Pro.local>
2024-06-28 15:55:26 -0500

    feature: http service implementation for weather

--- bmi_calculator/lib/main.dart
+++ bmi_calculator/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'input_page.dart';
-import 'result_page.dart';
+import 'results_page.dart';
 
 void main() => runApp(BMICalculator());
 
--- clima/lib/screens/loading_screen.dart
+++ clima/lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '/services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -11,12 +12,27 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void initState() {
     super.initState();
     getLocation();
+    getData();
   }
 
   void getLocation() async {
-    Location location = Location();
+    var location = Location();
     await location.getCurrentLocation();
     print('latitude: ${location.latitude} longitude: ${location.longitude}');
+  }
+
+  void getData() async {
+    var url = Uri.https('api.openweathermap.org', '/data/2.5/weather', {
+      'lat': '33.44',
+      'lon': '-94.04',
+      'exclude': 'hourly,daily',
+      'appid': '1f6ca21456731b9de44ce0ea03c478d0'
+    });
+
+    var response =
+        await http.get(url, headers: {'name': 'doodle', 'color': 'blue'});
+
+    print(response.body);
   }
 
   @override
